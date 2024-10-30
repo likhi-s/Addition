04d54b6ab14c7a80c8de37b8df771583aff1fc26
Likitha <likithasshivareddy@gmail.com>
2024-10-30 12:07:02 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=6;
+    int b=8;
+    int c=2;
+    int d=3;
+    int e=4;
+    int f=5;
+
+    int sum1=a+b;
+    int sum2=c+c;
+    int sum3=d+d;
+    int sum4=e+e;
+    int sum5=f+f;
+
+    cout<<"sum of the numbers"<<endl<<sum1<<endl<<sum2<<endl<<sum3<<endl<<sum4<<endl<<sum5<<endl;
+    return 0;
+
+}
